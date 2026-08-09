10a237e6cf410374a202e7945a9f88bbbfb54921
Vatsal <131441660+vatsal2641@users.noreply.github.com>
2026-08-09 20:34:36 +0530

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,43 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count_s = [0]*26
+        
+        if len(s1)<=len(s2):
+            larger = s2
+            smaller = s1
+        else:
+            return False
+        
+        for c in smaller: 
+            count_s[ord(c)-ord('a')]+=1
+        
+        n = len(larger)
+        k = len(smaller)
+
+        found = False
+        count_o = [0]*26
+        curr_str = larger[0:k]
+
+        for c in curr_str:
+            count_o[ord(c)-ord('a')]+=1
+        
+        for i in range(n-k+1):
+            indicator = True
+            
+            if count_s==count_o: 
+                found = True
+
+            
+            count_o[ord(larger[i]) - ord('a')]-=1
+            if i == (n-k):
+                continue
+            count_o[ord(larger[i+k]) - ord('a')]+=1
+
+           
+        if found == True:
+            return True
+        else: 
+            return False
+
+
+
