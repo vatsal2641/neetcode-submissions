12ad39c8b4a5abdffc94c906c8380ce240ede440
Vatsal <131441660+vatsal2641@users.noreply.github.com>
2026-08-11 17:06:20 +0530

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mini = float('inf')
+
+
+    def push(self, val: int) -> None:
+       
+        if self.mini>val:
+            self.mini = val
+            
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        if len(self.stack):
+            pop_val = self.stack.pop()
+       
+            if len(self.stack):
+                self.mini = min(self.stack)
+            else:
+                self.mini = float('inf')
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mini
