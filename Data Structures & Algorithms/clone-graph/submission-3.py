012921c675ce1cef563e092342d1de73bf735a29
Vatsal <131441660+vatsal2641@users.noreply.github.com>
2026-09-11 14:05:01 +0530

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return node
+
+        oldtonew = {node: Node(node.val)}
+
+        q = deque()
+        q.append(node)
+
+        while q:
+            curr_node = q.popleft()
+            for neighbor in curr_node.neighbors:
+                if neighbor not in oldtonew:
+                    q.append(neighbor)       #Never been visited, oldtonew does work for visited also
+                    oldtonew[neighbor] = Node(neighbor.val)
+
+                oldtonew[curr_node].neighbors.append(oldtonew[neighbor])
+        
+        return oldtonew[node]
